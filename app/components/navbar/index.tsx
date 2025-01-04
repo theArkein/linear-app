2c81eb93d311d139a348c0344afc7f6960432d55
Sarad Singh <theArkein.dev@gmail.com>
2025-01-04 12:28:49 +0545

    First draft of mocking linear.app

---
+++ app/components/navbar/index.tsx
@@ -0,0 +1,151 @@
+"use client"
+
+import * as React from "react"
+import Link from "next/link"
+
+import { cn } from "@/lib/utils"
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+import { Card } from "@/components/ui/card"
+import LinearAppIcon from "../icons/linear-app"
+import { Button } from "@/components/ui/button"
+
+// menuitems are
+// features, Method, Customers, Changelog, Pricing, Company, Contact
+
+const menuItems = [
+    {
+        title: "Features",
+        href: "/features",
+    },
+    {
+        title: "Method",
+        href: "/method",
+    },
+    {
+        title: "Customers",
+        href: "/customers",
+    },
+    {
+        title: "Changelog",
+        href: "/changelog",
+    },
+    {
+        title: "Pricing",
+        href: "/pricing",
+    },
+    {
+        title: "Company",
+        href: "/company",
+    },
+    {
+        title: "Contact",
+        href: "/contact",
+    },
+]
+
+
+
+export function Navbar() {
+  return (
+    <Card className="max-w-screen-lg m-auto p-2 mt-4 px-4">
+        <NavigationMenu >
+            <NavigationMenuList>
+                {/* <NavigationMenuItem>
+                <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                    <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                    <li className="row-span-3">
+                        <NavigationMenuLink asChild>
+                        <a
+                            className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                            href="/"
+                        >
+                            <div className="mb-2 mt-4 text-lg font-medium">
+                            shadcn/ui
+                            </div>
+                            <p className="text-sm leading-tight text-muted-foreground">
+                            Beautifully designed components built with Radix UI and
+                            Tailwind CSS.
+                            </p>
+                        </a>
+                        </NavigationMenuLink>
+                    </li>
+                    <ListItem href="/docs" title="Introduction">
+                        Re-usable components built using Radix UI and Tailwind CSS.
+                    </ListItem>
+                    <ListItem href="/docs/installation" title="Installation">
+                        How to install dependencies and structure your app.
+                    </ListItem>
+                    <ListItem href="/docs/primitives/typography" title="Typography">
+                        Styles for headings, paragraphs, lists...etc
+                    </ListItem>
+                    </ul>
+                </NavigationMenuContent>
+                </NavigationMenuItem> */}
+                <NavigationMenuItem key={"logo"}>
+                    <NavigationMenuLink className={""}>
+                        <LinearAppIcon size={16} height={16} />
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+
+                {
+                    menuItems.map((item) => (
+                        <NavigationMenuItem key={item.title}>
+                            <Link href={item.href} legacyBehavior passHref>
+                                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                {item.title}
+                                </NavigationMenuLink>
+                            </Link>
+                        </NavigationMenuItem>
+                    ))
+                }
+                <NavigationMenuItem className="flex space-x-3">
+                    <Button className="group bg-neutral-800 py-1 text-foreground hover:bg-zinc-700" asChild>
+                        <Link href="/signup">
+                            <span>Log in</span>
+                            <span className="bg-neutral-900 group-hover:bg-zinc-800  text-xs flex justify-center  items-center text-center w-5 h-5 rounded-sm">L</span>
+                        </Link>
+                    </Button>
+                    <Button className="bg-zinc-200 py-1" asChild>
+                        <Link href="/signup">Sign up</Link>
+                    </Button>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+
+        </NavigationMenu>
+    </Card>
+  )
+}
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = "ListItem"
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,371 @@
 import Image from "next/image";
+import { Navbar } from "./components/navbar";
+import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
+import DashboardPreview from "./public/dashboard.png"
+
+import  { Customer1, Customer2, Customer3, Customer4, Customer5, Customer6 } from "./components/icons/customers";
+import feature1 from "./public/feature1.png"
+import feature2 from "./public/feature2.png"
+import feature3 from "./public/feature3.png"
+import carousel1 from "./public/carousel1.png"
+import carousel2 from "./public/carousel2.png"
+import carousel3 from "./public/carousel3.png"
+import carousel4 from "./public/carousel4.png"
+import carousel5 from "./public/carousel5.png"
+import carousel6 from "./public/carousel6.png"
+
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Separator } from "@/components/ui/separator";
+import LinearAppIcon from "./components/icons/linear-app";
+
+const customers = [Customer1, Customer2, Customer3, Customer4, Customer5, Customer6]
+
+const features = [
+  {
+    title: <span>
+            Purpose-built for <br/>product development
+          </span>,
+    thumbnail: feature1,
+    descriptions: [
+      "Linear was developed with a specific purpose: to empower product teams to do their best work. Every aspect is intentionally designed to help teams focus on what they do best: Planning, building, and shipping great products.",
+      "Because of its fit-to-purpose design, Linear is incredibly easy to use, but grows more powerful as you scale. It’s opinionated where it needs to be, but provides enough flexibility to adapt to your team’s unique way of working.",
+      "We believe that this approach creates a better way to build products. And more than 10,000 product teams around the globe – from early-stage startups to public companies – agree.",
+    ],
+    quote: "“We’d tried many tools before Linear but none of them felt like they were made for the way we work. Linear was a breath of fresh air - speedy, snappy, and a pleasure to use.”",
+    customer: Customer4
+  },
+  {
+    title: "Designed to move fast",
+    thumbnail: feature2,
+    descriptions: [
+      "Linear is built in pursuit of high-performance. With its keyboard-first design, realtime sync, and zero-friction workflows, it delivers a focused experience ideal for fast-paced development environments.",
+      "Linear’s obsessive focus on speed not only results in improved developer productivity, but also enables teams to move faster.",
+      "Companies that switch to Linear report a significant uplift in newly filed tickets, vastly reduced issue closing times, and products that ship faster.",
+      "For us, speed is not just an afterthought. It’s our most important feature."
+    ],
+    quote: "“Even as we continue to grow, we’re moving faster because Linear keeps our workflows simple.”",
+    customer: Customer5
+  },
+  {
+    title: "Crafted to perfection",
+    thumbnail: feature3,
+    descriptions: [
+      "We believe that there is a lost art of building software. A craftsmanship that yields products of an exceptional, almost magical, quality.",
+      "It’s difficult to describe this quality with words and impossible to measure it in numbers, but you can feel it when it’s there. You know it when you experience it.",
+      "Achieving this product quality is a function of both the talent of its creators and the tools at their disposal. Linear was built in pursuit of giving the best product teams a tool that matches their skill and ambition.",
+      "A tool with unparalleled speed, precision-engineered robustness, and pixel-perfect UI. A tool that abstracts away any impurity that stands in the way between the creator and their craft. A tool that finally lets product teams focus on what matters most:",
+      "Bringing back product experiences of the highest level of quality.",
+    ],
+    quote: "“In a world of clunky, slow, and bloated tools, Linear reverses the script on all three and adds an unrivaled level of delight and polish.”",
+    customer: Customer6
+  }
+]
+
+const carousels = [
+  {
+    title: <span>Build what customers <br/> actually want </span>,
+    head: "Customer Requests",
+    thumbnail: carousel1,
+  },
+  {
+    title: <span>Automate pull requests <br/> and commit workflows</span>,
+    head: "Powerful git workflows",
+    thumbnail: carousel2,
+  },
+  {
+    title: <span>Move Product work<br/>forward form anywhere</span>,
+    head: "Linear Mobile",
+    thumbnail: carousel3,
+  },
+  {
+    title: <span>Turn workspace requests <br/>into actionable issues</span>,
+    head: "Linear Asks",
+    thumbnail: carousel4,
+
+  },
+  {
+    head: "Linear Integrations",
+    title: <span>Connect Linear to the <br/>tools you already use</span>,
+    thumbnail: carousel5,
+  },
+  {
+    head: "Figma integrations",
+    title: <span>Bridge the gap between <br/> engineering and design</span>,
+    thumbnail: carousel6,
+  },
+  {
+    head: "Built for developers",
+    title: <span>Build your own add-<br/>ons with the linear API</span>,
+    thumbnail: carousel3,
+  }
+]
+
+const footerMenus = [
+  {
+    title: "Features",
+    items: [
+      "Plan",
+      "Build",
+      "Insights",
+      "Customer Requests",
+      "Linear Asks",
+      "Security",
+      "Mobile"
+    ]
+  },
+  {
+    title: "Product",
+    items: [
+      "Pricing",
+      "Method",
+      "Integrations",
+      "Changelog",
+      "Documentation",
+      "Download",
+      "Switch"
+    ]
+  },
+  {
+    title: "Company",
+    items: [
+      "About",
+      "Customers",
+      "Careers",
+      "Blog",
+      "README",
+      "Quality",
+      "Brand"
+    ]
+  },
+  {
+    title: "Resources",
+    items: [
+      "API",
+      "Status",
+      "Startups",
+      "Report issue",
+      "DPA",
+      "Privacy",
+      "Terms"
+    ]
+  },
+  {
+    title: "Connect",
+    items: [
+      "Contact us",
+      "Community",
+      "X (Twitter)",
+      "GitHub",
+      "YouTube"
+    ]
+  }
+];
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <Navbar/>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+     <div className="max-w-screen-lg mx-auto mt-24">
+        <h1 className=" text-5xl font-bold">
+          <div>
+            Linear is a purpose-built tool for 
+          </div>
+          <div>
+            planning and building products
+          </div>
+        </h1>
+        <p className=" mt-6 mb-10 text-xl font-semibold text-muted-foreground">
+          Meet the system for modern software development. 
+          <br/>
+          Streamline issues, projects, and product roadmaps.</p>
+        <div className="flex gap-7 mt-10">
+          <Button className="font-semibold bg-zinc-200">Start Building</Button>
+          <Button variant="ghost" className="font-semibold hover:bg-zinc-700">
+            <span >Introducing</span>
+            <span className="text-zinc-400">Customer Requests</span>
+          </Button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+     </div>
+
+     <div className=" w-11/12 m-auto">
+      <Image 
+      className="relative left-28 -top-12 -z-10"
+      src={DashboardPreview} 
+      alt="Dashboard"
+            sizes="100vw"
+            // Make the image display full width
+            style={{
+              width: '100%',
+              height: 'auto',
+            }} />
+     </div>
+
+     <div className="max-w-screen-lg mx-auto mb-40 ">
+        <div className="flex flex-col items-center mt-10">
+          <p className=" text-xl font-semibold">Powering the world’s best product teams.</p>
+          <p className="text-xl font-semibold text-muted-foreground">From next-gen startups to established enterprises.</p>
+        </div>
+
+      <div className="group relative hover:cursor-pointer">
+        <div className=" group-hover:blur-sm mt-20 mx-12 flex flex-wrap justify-between gap-y-20 gap-x-16">
+          {
+            customers.map((Customer, index) => (
+              <div key={index} className="w-48">
+                <div className="w-40">
+                  <Customer />
+                </div>
+              </div>
+            ))
+          }
+        </div>
+        <div className="w-full h-full absolute justify-center items-center top-0 hidden group-hover:flex">
+          <Button className="rounded-full bg-zinc-700 text-foreground hover:bg-zinc-700 px-8 border-zinc-600 border">Meet our customers</Button>
+        </div>
+      </div>
+     </div>
+
+    <div className="max-w-screen-lg mx-auto">
+      <div className="flex items-end mb-20">
+        <div className=" w-1/2">
+          <h2 className=" text-5xl font-semibold leading-tight">Made for modern <br/> product teams</h2>
+        </div>
+        <div className=" w-1/2">
+          <p className="text-muted-foreground">
+            Linear is shaped by the practices and principles that distinguish world-class product teams from the rest: relentless focus, fast execution, and a commitment to the quality of craft.
+            <Button variant="ghost" className="m-o p-0 text-foreground font-bold hover:bg-background"> &nbsp;Make the switch</Button>
+          </p>
+        </div>
+      </div>
+
+      <div className="flex gap-8">
+        {
+          features.map((feature, index) => (
+            <Dialog key={index}>
+                <DialogTrigger asChild>
+                  <div className="group flex flex-col justify-between border border-zinc-800 items-center gap-10 bg-[#08090a] w-full rounded-3xl p-6 hover:cursor-pointer opacity-75 hover:opacity-100" key={index}>
+                      <Image src={feature.thumbnail} alt="Feature1" 
+                        sizes="100vw"
+                        // Make the image display full width
+                        style={{
+                          width: '80%',
+                          height: '80%',
+                        }} 
+                      />
+                    <div className="flex justify-between items-center gap-4">
+                    <h3 className=" text-xl font-semibold">{feature.title}</h3>
+                      <span className=" group-hover:bg-zinc-900 text-xl rounded-full h-10 w-10 border-neutral-800 border flex justify-center items-center">+</span>
+                    </div>
+                  </div>
+                </DialogTrigger>
+              <DialogContent className=" max-w-screen-lg my-10 overflow-x-hidden max-h-svh overflow-y-scroll pb-20">
+                    <div className="overflow-y-scroll h-full flex items-center flex-col">
+                    <Image 
+                    className="relative"
+                    src={feature.thumbnail} alt="Feature1" 
+                        sizes="100vw"
+                        // Make the image display full width
+                        style={{
+                          width: '60%',
+                          height: '60%',
+                        }} 
+                      />
+                      <div className=" px-28">
+                        <h3 className=" text-5xl font-semibold mb-8">
+                         {feature.title}
+                        </h3>
+                        <div className="flex flex-col gap-8">
+                          {
+                            feature.descriptions.map((description, index) => (
+                              <p key={index} className="text-muted-foreground ">{description}</p>
+                            ))
+                          }
+                        </div>
+                        <Separator className="my-12" />
+                        <p className=" mx-12 text-center text-xl">{feature.quote}</p>
+                        <div className="w-60 m-auto mt-10">
+                          <feature.customer />
+                        </div>
+                        </div>
+                    </div>
+
+              </DialogContent>
+            </Dialog>
+          ))
+        }
+      </div>
     </div>
+    <div className="max-w-screen-lg mx-auto mt-20">
+      <div className="flex items-end mb-20">
+        <div className=" w-1/2">
+          <h2 className=" text-5xl font-semibold leading-tight">Made for modern <br/> product teams</h2>
+        </div>
+        <div className=" w-1/2">
+          <p className="text-muted-foreground">
+            Linear is shaped by the practices and principles that distinguish world-class product teams from the rest: relentless focus, fast execution, and a commitment to the quality of craft.
+            <Button variant="ghost" className="m-o p-0 text-foreground font-bold hover:bg-background"> &nbsp;Make the switch</Button>
+          </p>
+        </div>
+      </div>
+      <Carousel>
+        <CarouselContent >
+        {
+          carousels.map((carousel, index) => (
+            <CarouselItem className=" basis-1/3" key={index}>
+              <div className="group h-full flex flex-col justify-between border border-zinc-800 items-center gap-6 bg-[#08090a] w-full rounded-3xl hover:cursor-pointer opacity-75 hover:opacity-100" key={index}>
+                  <div className="w-4/5 relative h-72">
+                    <Image src={carousel.thumbnail} alt="Feature1" 
+                      fill
+                      style={{
+                        objectFit: 'cover', // cover, contain, none
+                      }}
+                    />
+                  </div>
+                  <div className="flex justify-between items-center gap-4 p-6 ">
+                    <h3 className=" text-xl font-semibold">{carousel.title}</h3>
+                    <span className=" group-hover:bg-zinc-900 text-xl rounded-full h-10 w-10 border-neutral-800 border flex justify-center items-center">+</span>
+                  </div>
+                </div>
+            </CarouselItem>
+            ))
+          }
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+      <div className="flex gap-8">
+       
+      </div>
+      </div>
+
+      <div  className="bg-neutral-900 mt-20">
+        <div className="flex justify-between max-w-screen-lg mx-auto py-20">
+          <div>
+            <LinearAppIcon size={16} height={16} />
+          </div>
+          {footerMenus.map((menu, index) => (
+            <div key={index} className="flex flex-col gap-4 pt-0 p-8">
+              <h3 className="text-sm font-semibold">{menu.title}</h3>
+              <ul className="flex flex-col gap-2">
+                {
+                  menu.items.map((item, index) => (
+                    <li key={index} className="text-sm text-muted-foreground">{item}</li>
+                  ))
+                }
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
